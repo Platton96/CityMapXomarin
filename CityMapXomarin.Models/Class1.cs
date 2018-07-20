686f1eab940c0e4beb2cda682718093688569d72
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-07-20 15:36:59 +0300

    Create models

--- CityMapXomarin.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CityMapXomarin.Models
-{
-    public class Class1
-    {
-    }
-}
---
+++ CityMapXomarin.Models/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace CityMapXomarin.Models
+{
+    [DataContract]
+    public class Data
+    {
+        [DataMember(Name = "photos")]
+        public IEnumerable<CityModel> Cities { get; set; }
+    }
+}
